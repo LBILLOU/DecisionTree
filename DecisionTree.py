8a53b497f1c67bf34acd3403f796af3bb4eba0c5
Lo Billou <lbillou@MacBook-Pro-de-Lo.local>
2019-03-10 22:47:57 +0100

    Functions reorganized

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,54 +1,27 @@
 import os
 import pandas as pd
 
-print ('--------init--------')
-
-# csv file selection by user
-"""print ('>>> Import data from csv file, which file? (filename.csv)')
-filename = input()
-while os.path.isfile(filename) is False:
-    print('ERROR -> Please choose a csv file in current directory.')
-    filename = input()"""
-
-filename = "fruits.csv" # for testing
-impCSV = pd.read_csv(filename, header=0)
-
-print('Imported Data Shape')
-print(impCSV.shape)
-
-print('Imported Data columns')
-impColNames = list(impCSV.columns)
-print(impColNames)
-
-"""print('>>> Which column would you like to predict?')
-userTarget = input()
-while userTarget not in impColNames:
-    print('ERROR -> Please choose a column you like to predict from the following list:')
-    print(impColNames)
-    userTarget = input()
-
-target = impCSV[userTarget] """
-target = 'label'
-targetId = impCSV.columns.get_loc(target)
+def getUserInput(question):
+    # Funtion to retrieve input from user by asking an appropriate question
+    print('')
+    print(str(question))
+    userInput = input()
+    print('')
+    return userInput
 
 def uniqueValues(dataf, col):
+    # Function to return unique values of a given column from a dataframe
+    # using column index
     return dataf[dataf.columns[col]].unique().tolist()
 
-print("> Function *** uniqueValues")
-print("out --> " + str(uniqueValues(impCSV, targetId)))
-print(type(uniqueValues(impCSV, targetId)))
-
-def countValues(dataf):
+def countValues(dataf, targetId):
+    # Function to return the occurence of a value within a dataframe column
     output = {}
     for value in dataf[dataf.columns[targetId]]:
         if value not in output:
             output[value] = 0
         output[value] += 1
     return output
-
-print("> Function *** countValues")
-print("out --> " + str(countValues(impCSV)))
-print(type(countValues(impCSV)))
 
 class Question():
     def __init__(self, column, value):
@@ -62,23 +35,18 @@ class Question():
         else: operator = '=='
         return "Is column %s %s %s ?" % (self.column, operator, str(self.value))
 
+#
+#
+#
+
     def match(self, dataf):
         value = dataf[list(dataf)[self.column]].values
         if isinstance(self.value, int) or isinstance(self.value, float):
             return value >= self.value
         else: return value == self.value # return type ndarray...
-
-print("> Class *** Question")
-q = Question(0, 'Green')
-print(q)
-row = impCSV.loc[2, :].to_frame().transpose()
-# print(row)
-print("> Function *** Question.match")
-print("out --> " + str(q.match(row)))
-print(type(q.match(row)))
-
-#for i in impCSV:
-print(impCSV.loc[1].to_frame().transpose())
+#
+#
+#
 
 def branchsplit(dataf, question):
     trueRows = pd.DataFrame(columns = dataf.columns.values)
@@ -91,15 +59,8 @@ def branchsplit(dataf, question):
         else: falseRows = pd.concat([falseRows, datafRow])
     return trueRows, falseRows
 
-print("> Function *** branchsplit")
-true , false = branchsplit(impCSV, q)
-print("out --> " )
-print(true)
-print(false)
-print(type(true))
-
-def giniImp(dataf):
-    dfValues = countValues(dataf)
+def giniImp(dataf, targetId):
+    dfValues = countValues(dataf, targetId)
     impurity = 1
     for key in dfValues:
         keyProb = float(dfValues[key]) / dataf.shape[0]
@@ -108,24 +69,15 @@ def giniImp(dataf):
         return print("Error, negative impurity")
     return impurity
 
-print("> Function *** giniImp")
-print("out --> " + str(giniImp(false)))
-print(type(giniImp(true)))
-
-def infoGain(left, right, currentImp):
+def infoGain(left, right, currentImp, targetId):
     p = float(len(left)) / (len(left) + len(right))
-    return currentImp - p * giniImp(left) - (1 - p) * giniImp(right)
+    return currentImp - p * giniImp(left, targetId) - (1 - p) * giniImp(right, targetId)
 
-print("> Function *** infoGain")
-out = infoGain(true, false, giniImp(impCSV))
-print("out --> " + str(out))
-print(type(out))
-
-def findBestQuestion(dataf):
+def findBestQuestion(dataf, targetId):
     # Seeking best question through dataFrame
     bestGain = 0
     bestQuestion = None
-    currentImp = giniImp(dataf)
+    currentImp = giniImp(dataf, targetId)
     colNumber = dataf.shape[1]
 
     for col in range(colNumber):
@@ -140,7 +92,7 @@ def findBestQuestion(dataf):
                     continue
 
                 # Calculating question gain
-                gain = infoGain(trueRows, falseRows, currentImp)
+                gain = infoGain(trueRows, falseRows, currentImp, targetId)
 
                 # Choosing question with the best gain
                 if gain >= bestGain:
@@ -148,14 +100,10 @@ def findBestQuestion(dataf):
 
     return bestGain, bestQuestion
 
-print("> Function *** findBestQuestion")
-out = findBestQuestion(impCSV)
-print("out --> " + str(out))
-print(type(out))
 
 class Leaf:
-    def __init__(self, dataf):
-        self.predictions = countValues(dataf)
+    def __init__(self, dataf, targetId):
+        self.predictions = countValues(dataf, targetId)
 
 class Node:
     def __init__(self, question, trueBranch, falseBranch):
@@ -163,19 +111,14 @@ class Node:
         self.trueBranch = trueBranch
         self.falseBranch = falseBranch
 
-def buildTree(dataf):
-    gain, question = findBestQuestion(dataf)
+def buildTree(dataf, targetId):
+    gain, question = findBestQuestion(dataf, targetId)
     if gain == 0:
-        return Leaf(dataf)
+        return Leaf(dataf, targetId)
     trueRows, falseRows = branchsplit(dataf, question)
-    trueBranch = buildTree(trueRows)
-    falseBranch = buildTree(falseRows)
+    trueBranch = buildTree(trueRows, targetId)
+    falseBranch = buildTree(falseRows, targetId)
     return Node(question, trueBranch, falseBranch)
-
-print("> Function *** buildTree")
-my_tree = buildTree(impCSV)
-print("out --> " + str(out))
-print(type(out))
 
 def printTree(node, spacing = ""):
     if isinstance(node, Leaf):
@@ -187,9 +130,6 @@ def printTree(node, spacing = ""):
     print (spacing + '--> False:')
     printTree(node.falseBranch, spacing + "  ")
 
-print("> Function *** printTree")
-printTree(my_tree)
-
 def classify(row, node):
     if isinstance(node, Leaf):
         return node.predictions
@@ -198,11 +138,6 @@ def classify(row, node):
     else:
         return classify(row, node.falseBranch)
 
-print("> Function *** testTree")
-test = classify(impCSV.loc[2,:].to_frame().transpose(), my_tree)
-print("out --> " + str(test))
-print(type(test))
-
 def printLeaf(counts):
     total = sum(counts.values()) * 1.0
     probs = {}
@@ -210,7 +145,95 @@ def printLeaf(counts):
         probs[lbl] = str(int(counts[lbl] / total * 100)) + "%"
     return probs
 
-filename2 = "testingFruits.csv" # for testing
-impCSV2 = pd.read_csv(filename, header=0)
-for index, row in impCSV2.iterrows():
-    print ("Actual: %s. Predicted: %s " % (row[targetId], printLeaf(classify(row.to_frame().transpose(), my_tree))))
+# MAIN
+
+def main():
+    print('--------------------------------')
+    print('--- DECISION TREE VISUALIZER ---')
+    print('--------------------------------')
+
+    # csv file selection by user
+    filename = getUserInput('>>> Import your csv file (filename.csv) :')
+    while os.path.isfile(filename) is False:
+        filename = getUserInput('>>> Error, file not found. Please try again :')
+
+    # filename = "fruits.csv" # for testing
+    impCSV = pd.read_csv(filename, header=0)
+
+    print('Imported Data Shape : ', end = '')
+    print(impCSV.shape)
+
+    print('Imported Data columns : ', end = '')
+    impColNames = list(impCSV.columns)
+    print(impColNames)
+
+    userTarget = getUserInput('>>> Which column from the above would you like to predict?')
+    while userTarget not in impColNames:
+        userTarget = getUserInput('>>> Error, Please choose a column you like to predict from the following list: ' + str(print(impColNames)))
+
+    # target = impCSV[userTarget]
+
+    target = 'label'
+    targetId = impCSV.columns.get_loc(target)
+
+    print("> Function *** uniqueValues")
+    print("out --> " + str(uniqueValues(impCSV, targetId)))
+    print(type(uniqueValues(impCSV, targetId)))
+
+    print("> Function *** countValues")
+    print("out --> " + str(countValues(impCSV, targetId)))
+    print(type(countValues(impCSV, targetId)))
+
+    print("> Class *** Question")
+    q = Question(0, 'Green')
+    print(q)
+    row = impCSV.loc[2, :].to_frame().transpose()
+
+    print("> Function *** Question.match")
+    print("out --> " + str(q.match(row)))
+    print(type(q.match(row)))
+
+    #for i in impCSV:
+    print(impCSV.loc[1].to_frame().transpose())
+
+    print("> Function *** branchsplit")
+    true , false = branchsplit(impCSV, q)
+    print("out --> " )
+    print(true)
+    print(false)
+    print(type(true))
+
+    print("> Function *** giniImp")
+    print("out --> " + str(giniImp(false, targetId)))
+    print(type(giniImp(true, targetId)))
+
+    print("> Function *** infoGain")
+    out = infoGain(true, false, giniImp(impCSV, targetId), targetId)
+    print("out --> " + str(out))
+    print(type(out))
+
+    print("> Function *** findBestQuestion")
+    out = findBestQuestion(impCSV, targetId)
+    print("out --> " + str(out))
+    print(type(out))
+
+    print("> Function *** buildTree")
+    my_tree = buildTree(impCSV, targetId)
+    print("out --> " + str(out))
+    print(type(out))
+
+    print("> Function *** printTree")
+    printTree(my_tree)
+
+    print("> Function *** testTree")
+    test = classify(impCSV.loc[2,:].to_frame().transpose(), my_tree)
+    print("out --> " + str(test))
+    print(type(test))
+
+    filename2 = "testingFruits.csv" # for testing
+    impCSV2 = pd.read_csv(filename, header=0)
+    for index, row in impCSV2.iterrows():
+        print ("Actual: %s. Predicted: %s " % (row[targetId], printLeaf(classify(row.to_frame().transpose(), my_tree))))
+
+
+main()
